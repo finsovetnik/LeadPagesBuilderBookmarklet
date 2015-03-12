db7741c57ca5af8f24f73571b78c6b295d0d39a5
Mon Lu <mon@ave81.com>
2015-03-12 10:03:38 -0500

    Changed highlight color to LP green and text to white

--- builderKeybinding.js
+++ builderKeybinding.js
@@ -18,7 +18,7 @@ javascript: void function() {
 
 	style.type = 'text/css';
 
-	styleArr.push('.currentInteractiveElement { background-color: #abd2e9 !important; }');
+	styleArr.push('.currentInteractiveElement { background-color: #1ec279; color: #fff; !important; }');
 
 	if (style.styleSheet){
 	  style.styleSheet.cssText = styleArr.join('');
